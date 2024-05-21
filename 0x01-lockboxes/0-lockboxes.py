dc8872d84f8b887fc4e7e6ac9ec730d36e0e82c0
Kehinde Opaleye <opaleyekehinde70@gmail.com>
2024-05-21 03:54:44 -0500

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,30 +1,49 @@
 #!/usr/bin/python3
+"""
+This module provides a function to determine whether all the locked boxes in a list can be opened.
+The boxes are numbered sequentially from 0 to n-1, and each box contains keys that can open other boxes.
+The goal is to figure out if all boxes can eventually be opened starting from box 0.
+"""
 
-def can_unlock_all(boxes):
+def canUnlockAll(boxes):
     """
-    Determines if all boxes can be opened.
+    Determines if all the boxes can be opened starting from the first box.
+    
+    This function uses a breadth-first search (BFS) approach to explore the boxes.
+    It begins with the first box (index 0) and uses a queue to manage the boxes
+    that need to be checked. As keys are found, they are used to open new boxes
+    and add them to the queue for further exploration.
 
     Args:
-        boxes (list of lists): A list of lists representing locked boxes.
-            Each box may contain keys to other boxes.
+        boxes (list of lists): A list where each element is a list of integers representing keys
+                               to other boxes. The index of the list represents the box number.
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
-    """
-    if not boxes:
-        return False
 
-    visited = set()
-    queue = [0]
+    Example:
+        boxes = [[1], [2], [3], [4], []]
+        canUnlockAll(boxes) -> True
+
+        boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+        canUnlockAll(boxes) -> True
+
+        boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+        canUnlockAll(boxes) -> False
+    """
+    from collections import deque
+
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    queue = deque([0])
 
     while queue:
-        current_box = queue.pop(0)
-        visited.add(current_box)
-
-        for key in boxes[current_box]:
-            if key not in visited and key < len(boxes):
+        current = queue.popleft()
+        for key in boxes[current]:
+            if key < n and not opened[key]:
+                opened[key] = True
                 queue.append(key)
-                visited.add(key)  # Add the key to visited to avoid revisiting
 
-    return len(visited) == len(boxes)
+    return all(opened)
 
