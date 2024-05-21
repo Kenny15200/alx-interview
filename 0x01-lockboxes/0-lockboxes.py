271fbc3a2dadfff88a397cd675167eab7f788471
Kehinde Opaleye <opaleyekehinde70@gmail.com>
2024-05-21 03:56:59 -0500

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """
-This module provides a function to determine whether all the locked boxes in a list can be opened.
-The boxes are numbered sequentially from 0 to n-1, and each box contains keys that can open other boxes.
-The goal is to figure out if all boxes can eventually be opened starting from box 0.
+Module to determine if all boxes can be opened.
+Each box may contain keys to other boxes. The function checks if starting from the
+first box, all other boxes can eventually be opened.
 """
+
+from collections import deque
 
 def canUnlockAll(boxes):
     """
     Determines if all the boxes can be opened starting from the first box.
-    
+
     This function uses a breadth-first search (BFS) approach to explore the boxes.
     It begins with the first box (index 0) and uses a queue to manage the boxes
     that need to be checked. As keys are found, they are used to open new boxes
     and add them to the queue for further exploration.
 
     Args:
-        boxes (list of lists): A list where each element is a list of integers representing keys
-                               to other boxes. The index of the list represents the box number.
+        boxes (list of list of int): A list where each element is a list of integers representing keys
+                                     to other boxes. The index of the list represents the box number.
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
@@ -31,8 +33,6 @@ def canUnlockAll(boxes):
         boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
         canUnlockAll(boxes) -> False
     """
-    from collections import deque
-
     n = len(boxes)
     opened = [False] * n
     opened[0] = True
